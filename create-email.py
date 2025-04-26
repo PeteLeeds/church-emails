34ef6ff9b860cd20e6cf0f9b52cff0322d647e40
PeteLeeds <peter_clark@talktalk.net>
2025-04-26 19:59:22 +0100

    Add basic formatting

--- create-email.py
+++ create-email.py
@@ -4,4 +4,9 @@ import requests
 text = requests.get("https://www.achurchnearyou.com/church/18109/service-and-events/feed/").text
 
 calendar = icalendar.Calendar.from_ical(text)
-print(calendar.events[0])
+
+for event in calendar.events:
+    print(event.get('SUMMARY'), '\n')
+    print(f"{event.get('LOCATION')}, {event.get('DTSTART').dt} - {event.get('DTEND').dt}\n")
+    print(event.get('DESCRIPTION'))
+    print('--------------')
