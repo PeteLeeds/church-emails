76fca6e064acb3062b4837c9dc4cf79c92cb5f80
PeteLeeds <peter_clark@talktalk.net>
2025-05-02 19:53:14 +0100

    Only get events within last 7 days

--- create-email.py
+++ create-email.py
@@ -1,12 +1,17 @@
 import icalendar
 import requests
+from datetime import datetime, timezone
 
 text = requests.get("https://www.achurchnearyou.com/church/18109/service-and-events/feed/").text
 
 calendar = icalendar.Calendar.from_ical(text)
 
 for event in calendar.events:
+    start_time = event.get('DTSTART').dt
+    now = datetime.now(timezone.utc)
+    if ((start_time - now).days > 7):
+        continue
     print(event.get('SUMMARY'), '\n')
-    print(f"{event.get('LOCATION')}, {event.get('DTSTART').dt} - {event.get('DTEND').dt}\n")
+    print(f"{event.get('LOCATION')}, {start_time} - {event.get('DTEND').dt}\n")
     print(event.get('DESCRIPTION'))
     print('--------------')
