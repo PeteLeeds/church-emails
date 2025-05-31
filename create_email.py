e6d87a1052e84198192d20f48fa4feb2b48d959d
PeteLeeds <peter_clark@talktalk.net>
2025-05-31 14:05:08 +0100

    Format using Black

--- create_email.py
+++ create_email.py
@@ -4,17 +4,19 @@ from datetime import datetime, timezone
 
 
 def create_email_message():
-    text = requests.get("https://www.achurchnearyou.com/church/18109/service-and-events/feed/").text
+    text = requests.get(
+        "https://www.achurchnearyou.com/church/18109/service-and-events/feed/"
+    ).text
 
     calendar = icalendar.Calendar.from_ical(text)
-    email =  ""
+    email = ""
     for event in calendar.events:
-        start_time = event.get('DTSTART').dt
+        start_time = event.get("DTSTART").dt
         now = datetime.now(timezone.utc)
-        if ((start_time - now).days > 7):
+        if (start_time - now).days > 7:
             continue
         email += f"{event.get('SUMMARY')}\n"
         email += f"{event.get('LOCATION')}, {start_time} - {event.get('DTEND').dt}\n\n"
         email += f"{event.get('DESCRIPTION')}\n"
-        email += '--------------\n'
+        email += "--------------\n"
     return email
--- send_email.py
+++ send_email.py
@@ -10,69 +10,67 @@ import base64
 from create_email import create_email_message
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly", "https://www.googleapis.com/auth/gmail.send"]
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send",
+]
 
 
 def main():
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-  try:
-    # Call the Gmail API
-    service = build("gmail", "v1", credentials=creds)
-    gmail_send_message(service)
+    try:
+        # Call the Gmail API
+        service = build("gmail", "v1", credentials=creds)
+        gmail_send_message(service)
 
-  except HttpError as error:
-    # TODO(developer) - Handle errors from gmail API.
-    print(f"An error occurred: {error}")
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f"An error occurred: {error}")
 
 
 def gmail_send_message(service):
-  try:
-    message = EmailMessage()
+    try:
+        message = EmailMessage()
 
-    email_message = create_email_message()
-    message.set_content(email_message)
+        email_message = create_email_message()
+        message.set_content(email_message)
 
-    message["To"] = os.environ.get('EMAIL_TO')
-    message["From"] = os.environ.get('EMAIL_FROM')
-    message["Subject"] = "Automated draft"
-    # encoded message
-    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+        message["To"] = os.environ.get("EMAIL_TO")
+        message["From"] = os.environ.get("EMAIL_FROM")
+        message["Subject"] = "Automated draft"
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-    create_message = {"raw": encoded_message}
-    # pylint: disable=E1101
-    send_message = (
-        service.users()
-        .messages()
-        .send(userId="me", body=create_message)
-        .execute()
-    )
-    print(f'Message Id: {send_message["id"]}')
-  except HttpError as error:
-    print(f"An error occurred: {error}")
-    send_message = None
-  return send_message
+        create_message = {"raw": encoded_message}
+        # pylint: disable=E1101
+        send_message = (
+            service.users().messages().send(userId="me", body=create_message).execute()
+        )
+        print(f'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        send_message = None
+    return send_message
 
 
 if __name__ == "__main__":
-  main()
+    main()
