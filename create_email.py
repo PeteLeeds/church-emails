3a6a09edb7a82f165ab2446890e4c96dd52259e3
PeteLeeds <peter_clark@talktalk.net>
2025-06-14 20:44:18 +0100

    Split up create_email_message into more logical functions

--- create_email.py
+++ create_email.py
@@ -7,6 +7,19 @@ CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL")
 CHURCH_ID = os.environ.get("CHURCH_ID")
 
 
+def in_next_week(event):
+    start_time = event.get("DTSTART").dt
+    now = datetime.now(timezone.utc)
+    return (start_time - now).days > 0 and (start_time - now).days < 6
+
+
+def format_event_for_email(event):
+    formatted_event = f"<h3>{event.get('SUMMARY')}</h3>"
+    formatted_event += f"<p><b>{event.get('LOCATION')}, {event.get('DTSTART').dt} - {event.get('DTEND').dt}</b></p>"
+    formatted_event += f"<p>{event.get('DESCRIPTION')}<p>"
+    return formatted_event
+
+
 def create_email_message():
     text = requests.get(
         f"https://www.achurchnearyou.com/church/{CHURCH_ID}/service-and-events/feed/"
@@ -16,12 +29,7 @@ def create_email_message():
     email = "<p>Please see below for this week's events:</p>"
     email += f"<p>For full details of all future events, please see our website on <a href=https://www.achurchnearyou.com/church/{CHURCH_ID}/>A Church Near You</a>"
     for event in calendar.events:
-        start_time = event.get("DTSTART").dt
-        now = datetime.now(timezone.utc)
-        if (start_time - now).days > 6 or (start_time - now).days < 0:
-            continue
-        email += f"<h3>{event.get('SUMMARY')}</h3>"
-        email += f"<p><b>{event.get('LOCATION')}, {start_time} - {event.get('DTEND').dt}</b></p>"
-        email += f"<p>{event.get('DESCRIPTION')}<p>"
+        if in_next_week(event):
+            email += format_event_for_email(event)
     email += f"<br/>If you have an event you would like to advertise, please contact the church at {CONTACT_EMAIL}"
     return email
